c25f97310094313ace95dc5efba555da453513f0
nachoaldamav <sr.drabx@gmail.com>
2022-08-28 20:06:48 +0200

    fix 🐛: (chore) Fix surprise

--- src/functions/currentBranch.ts
+++ src/functions/currentBranch.ts
@@ -1,13 +1,14 @@
 import chalk from "chalk";
 import { execa } from "execa";
 import ora from "ora";
+import open from "open";
 
 export async function getCurrentBranch() {
   return await execa("git", ["rev-parse", "--abbrev-ref", "HEAD"])
     .then((res) => res.stdout.trim())
-    .catch(() => {
+    .catch(async () => {
       ora(chalk.red("You are not inside a git repository!")).fail();
-      open("https://i.imgur.com/3xhYtW8.mp4");
+      await open("https://i.imgur.com/3xhYtW8.mp4", { wait: true });
       process.exit(1);
     });
 }
